fb644ca361b95ef7855e1e2a61314258d895860f
aurelienizl <aurelien.izoulet@epita.fr>
2023-01-02 19:39:55 +0000

    empty & exists ok

--- src/lists/linked-list/properties.c
+++ src/lists/linked-list/properties.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 #include "list.h"
 
+int is_empty(struct list *list)
+{
+    if(list->next == NULL)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int exists(struct list *list, int value)
+{
+    list = list->next; // Pass the sentinel 
+    while(list != NULL)
+    {
+        if(list->data == value)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 struct list *get_at(struct list *list, int index)
 {
     list = list->next; // Pass the sentinel
